040e36c463feb00b3aec89c68667cfb5395e02c7
AhmedAli270402 <ahmed@goccs.net>
2024-08-27 02:19:31 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,5 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-> ~/. streamlit/config.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
 maxUploadSize = 200\n\
 " > ~/.streamlit/config.toml
