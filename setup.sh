d48001102ac301c4312cacc9d57669d5c09ea922
AhmedAli270402 <ahmed@goccs.net>
2024-08-27 02:30:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
 mkdir -p ~/.streamlit/
+
 echo "\
-[genenal] \n\
+[general]\n\
 email = \"email@domain\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+port = \$PORT\n\
 maxUploadSize = 200\n\
 " > ~/.streamlit/config.toml
