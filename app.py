71b3babcfe3e4942d504997dc12b696f82c4354f
AhmedAli270402 <ahmed@goccs.net>
2024-09-04 19:15:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,15 +29,15 @@ if response.status_code == 200:
 
 else:
     print(f"Failed to retrieve the file. Status code: {response.status_code}")
-url = "https://placekey.nyc3.cdn.digitaloceanspaces.com/REI%20Sift-All%20data-08262024_standardized+placekeys.csv"
+brl = "https://placekey.nyc3.cdn.digitaloceanspaces.com/REI%20Sift-All%20data-08262024_standardized+placekeys.csv"
 
 # Make a request to get the CSV file
-response = requests.get(url)
+responses = requests.get(brl)
 
 # Check if the request was successful
-if response.status_code == 200:
+if responses.status_code == 200:
     # Read the CSV content using pandas
-    REI_local_path = StringIO(response.text)
+    REI_local_path = StringIO(responses.text)
 
 else:
     print(f"Failed to retrieve the file. Status code: {response.status_code}")    
