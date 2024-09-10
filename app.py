4604249604736d994b01939763b27f7973b12cdf
AhmedAli270402 <ahmed@goccs.net>
2024-09-10 04:15:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import json
 from placekey.api import PlacekeyAPI
 from io import StringIO
 
-placekey_api_key = "7fDmyQF5H2TNZSVIujmKzniJG94rVgfe"
+placekey_api_key = "SQPFI1y1Mw1njF0r7lzHC6CQMVTyPHH6"
 url = "https://placekey.nyc3.cdn.digitaloceanspaces.com/placekeys_standardized%20copy%207.csv"
 zrl = "https://placekey.nyc3.cdn.digitaloceanspaces.com/REI%20Sift-All%20data-08262024_standardized+placekeys%20(4).csv"
 
